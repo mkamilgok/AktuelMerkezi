9e433b09180be564b73db4390a1df1936110308f
mkamilgok <mkamilgok@gmail.com>
2020-08-27 15:14:35 +0300

    Implement fetching products from Bim supermarket

--- src/main/java/com/example/aktuelmerkezi/databaseLoader/BimDBUtilizer.java
+++ src/main/java/com/example/aktuelmerkezi/databaseLoader/BimDBUtilizer.java
@@ -1,8 +1,15 @@
 package com.example.aktuelmerkezi.databaseLoader;
 
+import com.example.aktuelmerkezi.model.Product;
 import com.example.aktuelmerkezi.repository.BrochureRepository;
 import com.example.aktuelmerkezi.repository.ProductRepository;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
 
 @Component
 public class BimDBUtilizer {
@@ -16,5 +23,28 @@ public class BimDBUtilizer {
     }
 
     public void fetchProductsAndBrochuresOfBim() {
+        String url = "https://www.bim.com.tr/default.aspx";
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(url).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        List<Element> productTemplates = doc.getElementsByClass("product big col-md-12 col-lg-12 col-xl-9 col-12");
+        productTemplates.addAll(doc.getElementsByClass("product col-xl-3 col-lg-3 col-md-4 col-sm-6  col-12"));
+        productTemplates.addAll(doc.getElementsByClass("product col-xl-3 col-lg-3 col-md-4 col-sm-6 col-12 LoadGroup0"));
+        productTemplates.forEach(element -> {
+            String name = element.getElementsByClass("title").text();
+            String price = element.getElementsByClass("text quantify").text()
+                            + element.getElementsByClass("number").text()
+                            + element.getElementsByClass("curr").text();
+            String link = "https://www.bim.com.tr" + element.getElementsByTag("a").attr("href");
+            String imgLink =  "https://www.bim.com.tr"
+                    + (element.getElementsByClass("img-fluid").attr("src").equals("") ?
+                    element.getElementsByClass("img-fluid").attr("xsrc") :
+                    element.getElementsByClass("img-fluid").attr("src"));
+            imgLink = imgLink.replaceAll(" ", "%20");
+            productRepository.save(new Product(name, price, link, imgLink, "Bim", "Bu hafta indirime girecekler"));
+        });
     }
 }
